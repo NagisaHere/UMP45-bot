1fc643b3568ff09b628cc1428e5ca1784fbdbc8c
NagisaHere <rw75000777@gmail.com>
2022-09-24 18:19:06 +1000

    Still need to fix images help

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Discord.Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
     ]
 })
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
+
+// Welcome message code below
+const welcomeChannelId = "1023065186099286036"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
+})
+
+
+
+// Welcome message code end
 
 client.login(process.env.TOKEN)
