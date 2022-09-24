9dc5ad524fe74498c904e310a15ca243fbce68e6
NagisaHere <rw75000777@gmail.com>
2022-09-24 12:51:39 +1000

    fixed env hopefully

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js")
-
-const TOKEN = "MTAyMzA0MzM4NTk4NjEyMTg1OQ.GEO_Eh.sItxihQ8Y3tVp0jouYx0h0m-x2v8qKxcv37__8"
+require("dotenv").config()
 
 const { Client, GatewayIntentBits } = require('discord.js');
 
@@ -15,4 +14,4 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.login(TOKEN)
+client.login(process.env.TOKEN)
